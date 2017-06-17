0df04372e09942f1e00e2989d848e396269cffad
nico <suttonnico2@gmail.com>
2017-06-17 15:17:41 -0300

    first upload

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ class mlp:
         M = np.max(n)
         #el primero es el numero de entradas a la red por eso N-1
         self.W = np.random.rand(self.N-1,M,M)
-
+        #W[capa,neurona
     def out(self, rn_in):
         for j in range(0,self.N-1):
             outl = np.zeros(self.n[j+1])
@@ -19,6 +19,10 @@ class mlp:
                 outl[i] = np.tanh(np.dot(self.W[j, i, :], rn_in))
                 rn_in = outl
         return rn_in
+
+    def train(self,train_data):
+        print 'not done yet'
+
 
 
 
@@ -28,13 +32,12 @@ class mlp:
 
 TP = np.array([[-1,-1],[-1,1],[1,-1],[1,1]])
 rn = mlp([2,2,1])
-print rn.W
-print rn.out([0,0,0,0])
+
 exit(100)
 T=[0,1,1,0]
 e = 1
 lr = 0.5
 
-for i in range(0, 100):
-    for t in range(0, 4):
+#for i in range(0, 100):
+#    for t in range(0, 4):
 
