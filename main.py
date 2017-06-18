495998f9fbb1ae7ddcc48e0c3e50d761c80e8cba
suttonnico <suttonnico2@gmail.com>
2017-06-18 10:37:17 -0300

    subida para cambiar compus

--- main.py
+++ main.py
@@ -10,31 +10,52 @@ class mlp:
         self.N = np.size(n)
         M = np.max(n)
         #el primero es el numero de entradas a la red por eso N-1
-        self.W = np.random.rand(self.N-1,M,M)
-        #W[capa,neurona
+        self.W = np.random.rand(self.N-1,M,M+1)     #+1 por bias
+        #W[capa,neurona,entradas]
     def out(self, rn_in):
         for j in range(0,self.N-1):
             outl = np.zeros(self.n[j+1])
             for i in range(0, self.n[j+1]):
-                outl[i] = np.tanh(np.dot(self.W[j, i, :], rn_in))
+                rn_in = np.append([1], rn_in)
+                outl[i] = self.F(np.dot(self.W[j, i, :], rn_in))
                 rn_in = outl
         return rn_in
 
-    def train(self,train_data):
-        print 'not done yet'
+
+    def F(self, input):
+        return np.tanh(input)
 
 
+    def dF(self, input):
+        return 1 - self.F(input) ** 2
 
 
+    def train(self,train_input, train_output):
+        training_cases = len(train_input[:, 1])
+        for i in range(0,training_cases):
+            O = self.out(train_input[i, :])
+            e = O - train_output[i]
+            total_error = self.sum_of_squares(e)
+            self. update(train_input,train_output,total_error)
+    def sum_of_squares(self,e):
+        sum_c = 0
+        for i in range(0,len(e)):
+            sum_c += e[i] ** 2
+        return sum_c
 
-    def update(self, total_error):
+    def update(self, train_input,train_output,total_error):
+
         self.W = self.W
 
-TP = np.array([[-1,-1],[-1,1],[1,-1],[1,1]])
-rn = mlp([2,2,1])
 
+
+train_data = np.array([[-1,-1],[-1,1],[1,-1],[1,1]])
+train_output = [0,1,1,0]
+rn = mlp([2,2,1])
+print rn.W[0,0,:]
+rn.train(train_data, train_output)
 exit(100)
-T=[0,1,1,0]
+
 e = 1
 lr = 0.5
 
