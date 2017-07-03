6929cc3225ca596664d11b4d849345ad9c8ce016
nico <suttonnico2@gmail.com>
2017-07-03 10:49:43 -0300

    ultimo con red de 4

--- main2.py
+++ main2.py
@@ -22,15 +22,30 @@ def PCA(lats, cantDim):
     auvec = auvec[:, idx]
     auvec = np.matrix.transpose(auvec)
     u = np.dot(auvec, lats)  # transformo los latidos normalizados al espacio PCA
+    plt.figure()
+    plt.plot(np.matrix.transpose(u)[0], label='Componentes totales')
     u[cantDim:2 * tam] = 0  # Hago cero las componentes mayores a cantDim
+    # plt.plot(np.matrix.transpose(u)[0], label='Componentes reducidas')
+    # plt.legend()
+    # plt.show()
+    #
     u = u[0:cantDim]
     u = np.matrix.transpose(u)
 
-    # u = np.matrix.transpose(u)
-
-    # lats = np.dot(np.matrix.transpose(auvec)[0:cantDim], u)
-    # lats = np.matrix.transpose(lats)
-
+    # latidosRec = np.dot(np.matrix.transpose(auvec), u)
+    # plt.figure()
+    # plt.plot(np.matrix.transpose(lats)[4011])
+    # plt.plot(np.matrix.transpose(latidosRec)[4011])
+    # plt.title('Latido original y reconstruido')
+    # plt.show()
+    # sum1= 0
+    # for i in range(0,len(lats)):
+    #     sum1 += (np.var(np.matrix.transpose(lats)[i]))
+    # sum2 = 0
+    # for i in range(0,len(lats)):
+    #     sum2 += (np.var(np.matrix.transpose(latidosRec)[i]))
+    # print sum1, sum2, 1-(sum1-sum2)/sum1
+    # exit(123)
     return u  # lats
 
 def kill_beat(latidos, latido_a_eliminar, cant_org, tam):
@@ -42,22 +57,7 @@ def kill_beat(latidos, latido_a_eliminar, cant_org, tam):
     return new_beats
 
 def load_data(N, V, S, J):
-    cantidades = sio.loadmat('cantidades.mat')
-    cantidades = cantidades['cantidades']
-    cantidades = cantidades[0]
-    N_all = cantidades[0]
-    V_all = cantidades[1]
-    S_all = cantidades[2]
-    J_all = cantidades[3]
-    all_N = sio.loadmat('all_N.mat')
-    all_N = all_N['all_N']
-    all_V = sio.loadmat('all_V.mat')
-    all_V = all_V['all_V']
-    all_S = sio.loadmat('all_S.mat')
-    all_S = all_S['all_S']
-    all_J = sio.loadmat('all_J.mat')
-    all_J = all_J['all_J']
-    tam = 125
+
 
     signals2, fields2 = wfdb.srdsamp('data/14172', channels=[0, 1])
     annotation = wfdb.rdann('data/14172', 'atr')
@@ -68,10 +68,10 @@ def load_data(N, V, S, J):
     # Transpongo las senales leidas para poder operar mas comodamente
     signals = np.matrix.transpose(signals2)
     picos[-1] = 0  # descarto el ultimo pico por si el latido esta interrumpido
-    tam = 125  # Tamano del latido que tomo a la izquierda y a la derecha de la anotacion
+    tam = 50  # Tamano del latido que tomo a la izquierda y a la derecha de la anotacion
     cantLatidos = sum(picos > tam)  # Cuento la cantidad de anotaciones en el segemento a analizar
     latidos = np.zeros([cantLatidos, 2 * tam])  # Reservo espacio para la matriz de datos cantLatidos x 2*tam
-    latidosNorm = np.zeros([cantLatidos, 2 * tam])  # Reservo espacio para la matriz de datos normalizados
+    latidosNorm = np.zeros([2 * tam, cantLatidos])  # Reservo espacio para la matriz de datos normalizados
     numLat = 0  # Inicializo en cero para procesar cada uno de los picos
 
     for i in picos:
@@ -79,12 +79,26 @@ def load_data(N, V, S, J):
             latidos[numLat] = signals[0][
                               i - tam:i + tam]  # tomo tam muestras a la izquerda y a la derecha de la anotacion
             numLat += 1  # incremento el numero de latidos
+
+    latidos = np.matrix.transpose(latidos)
     # El set de latidos esta en latidos y el largo de cada latido es de tam muestras
 
-    for i in range(0, cantLatidos):
-        latidosNorm[i] = latidos[i] - np.mean(latidos[i])
-        latidosNorm[i] /= np.std(latidosNorm[i])
+
+    medias = np.zeros(2 * tam)
+    desv = np.zeros(2 * tam)
+    for i in range(0, 2 * tam):
+        medias[i] = np.mean(latidos[i, :])
+        desv[i] = np.std(latidos[i, :])
+    for idx in range(0, 2 * tam):
+        latidosNorm[idx] = latidos[idx]  # - np.ones(cantLatidos) * medias[idx]
+    #        latidosNorm[idx] = latidosNorm[idx] / desv[idx]
+    latidosNorm = np.matrix.transpose(latidosNorm)
+    plt.figure()
+    plt.plot(latidosNorm[43655])
+    plt.title('Latido nodal prematuro')
+    plt.show()
     anotaciones = annotation.anntype
+    print anotaciones
     N_all = 10000
     V_all = 1000
     S_all = 300
@@ -94,8 +108,9 @@ def load_data(N, V, S, J):
     for i in range(0, N_all):
         for j in range(last_j, cantLatidos):
             if anotaciones[j] == 'N':
+                # print 'j',j,'i',i
                 all_N[i] = latidosNorm[j]
-                last_j = j
+                last_j = j +1
                 break
     all_V = np.zeros([V_all, 2 * tam])
     last_j = 0
@@ -103,7 +118,7 @@ def load_data(N, V, S, J):
         for j in range(last_j, cantLatidos):
             if anotaciones[j] == 'V':
                 all_V[i] = latidosNorm[j]
-                last_j = j
+                last_j = j +1
                 break
     all_S = np.zeros([S_all, 2 * tam])
     last_j = 0
@@ -111,7 +126,7 @@ def load_data(N, V, S, J):
         for j in range(last_j, cantLatidos):
             if anotaciones[j] == 'S':
                 all_S[i] = latidosNorm[j]
-                last_j = j
+                last_j = j +1
                 break
     all_J = np.zeros([J_all, 2 * tam])
     last_j = 0
@@ -119,7 +134,8 @@ def load_data(N, V, S, J):
         for j in range(last_j, cantLatidos):
             if anotaciones[j] == 'J':
                 all_J[i] = latidosNorm[j]
-                last_j = j
+                print j
+                last_j = j +1
                 break
 
     # all_N = PCA(all_N)
@@ -134,8 +150,8 @@ def load_data(N, V, S, J):
     LatidosNorm[N_all:N_all + V_all] = all_V
     LatidosNorm[N_all + V_all:N_all + V_all + S_all] = all_S
     LatidosNorm[N_all + V_all + S_all:N_all + V_all + S_all + J_all] = all_J
-    cantDim = 32
-    LatidosNorm = PCA(LatidosNorm, cantDim)
+    cantDim = 2 * tam  # 110
+    #LatidosNorm = PCA(LatidosNorm, cantDim)
     all_N = LatidosNorm[0:N_all]
     all_V = LatidosNorm[N_all:N_all + V_all]
     all_S = LatidosNorm[N_all + V_all:N_all + V_all + S_all]
@@ -164,6 +180,9 @@ def load_data(N, V, S, J):
         training_set_J[i] = all_J[j]
         all_J = kill_beat(all_J, j, J_all, cantDim)
         J_all -= 1
+    plt.figure()
+    plt.plot(all_N[0])
+    plt.show()
 
     test_set_N = all_N
     test_set_V = all_V
@@ -178,7 +197,7 @@ def load_data(N, V, S, J):
 
 
 [N, V, S, J, N_left, V_left, S_left, J_left, training_set_N, training_set_V, training_set_S, training_set_J, test_set_N,
- test_set_V, test_set_S, test_set_J] = load_data(10, 10, 10, 10)
+ test_set_V, test_set_S, test_set_J] = load_data(120, 40, 40, 40)
 tam = len(training_set_N[0])
 
 
@@ -196,7 +215,7 @@ for i in range(0, J):
     training_set = np.append(training_set, Instance(training_set_J[i], [0, 0, 0, 1]))
 print len(training_set)
 test_set = []
-for i in range(0, 100):
+for i in range(0, 300):
     test_set = np.append(test_set, Instance(test_set_N[i], [1, 0, 0, 0]))
 for i in range(0, 100):
     test_set = np.append(test_set, Instance(test_set_V[i], [0, 1, 0, 0]))
@@ -213,12 +232,12 @@ backpropagation(
     test_set,  # specify the test set
     cost_function,  # specify the cost function to calculate error
 
-    early_stop = 1,
+    early_stop= 10,
     ERROR_LIMIT=1e-3,  # define an acceptable error limit
-    max_iterations=10000,  # continues until the error limit is reach if this argument is skipped
+    max_iterations=100,  # continues until the error limit is reach if this argument is skipped
     print_rate=1000,
-    input_layer_dropout=0.1,  # Dropout fraction of the input layer
-    hidden_layer_dropout=0.1,  # Dropout fraction of in the hidden layer(s)
+    input_layer_dropout=0,  # Dropout fraction of the input layer
+    hidden_layer_dropout=0,  # Dropout fraction of in the hidden layer(s)
 )
 test_set = []
 wrn = 0
@@ -298,6 +317,86 @@ for i in range(1, J + 1):
     s = p == np.max(p)
     s = s[0]
     if s[3] == 1:
+        wrj = 1.0 * (wrj * (i - 1) + 1) / i
+    else:
+        wrj = 1.0 * (wrj * (i - 1)) / i
+print 'wrj', wrj
+
+settings = {"n_inputs": tam, "layers": [(36, sigmoid_function), (16, sigmoid_function), (1, sigmoid_function)]}
+network = NeuralNet(settings)
+training_set = []
+for i in range(0, N):
+    training_set = np.append(training_set, Instance(training_set_N[i], [1]))
+for i in range(0, V):
+    training_set = np.append(training_set, Instance(training_set_V[i], [0]))
+for i in range(0, S):
+    training_set = np.append(training_set, Instance(training_set_S[i], [0]))
+for i in range(0, J):
+    training_set = np.append(training_set, Instance(training_set_J[i], [0]))
+print len(training_set)
+test_set = []
+for i in range(0, 100):
+    test_set = np.append(test_set, Instance(test_set_N[i], [1]))
+for i in range(0, 100):
+    test_set = np.append(test_set, Instance(test_set_V[i], [0]))
+for i in range(0, 100):
+    test_set = np.append(test_set, Instance(test_set_S[i], [0]))
+for i in range(0, 100):
+    test_set = np.append(test_set, Instance(test_set_J[i], [0]))
+
+cost_function = sum_squared_error
+# test_set = training_set
+backpropagation(
+    network,  # the network to train
+    training_set,  # specify the training set
+    test_set,  # specify the test set
+    cost_function,  # specify the cost function to calculate error
+
+    early_stop=10,
+    ERROR_LIMIT=1e-3,  # define an acceptable error limit
+    max_iterations=10000,  # continues until the error limit is reach if this argument is skipped
+    print_rate=1000,
+    input_layer_dropout=0,  # Dropout fraction of the input layer
+    hidden_layer_dropout=0,  # Dropout fraction of in the hidden layer(s)
+)
+
+test_set = []
+wrn = 0
+for i in range(1, N_left + 1):
+    p = network.predict([Instance(test_set_N[i - 1])])
+    s = p == np.max(p)
+    s = s[0]
+    if p >= 0.5:
+        wrn = 1.0 * (wrn * (i - 1) + 1) / i
+    else:
+        wrn = 1.0 * (wrn * (i - 1)) / i
+print 'wrn', wrn
+wrv = 0
+for i in range(1, V_left + 1):
+    p = network.predict([Instance(test_set_V[i - 1])])
+    s = p == np.max(p)
+    s = s[0]
+    if p <= 0.5:
+        wrv = 1.0 * (wrv * (i - 1) + 1) / i
+    else:
+        wrv = 1.0 * (wrv * (i - 1)) / i
+print 'wrv', wrv
+wrs = 0
+for i in range(1, S_left + 1):
+    p = network.predict([Instance(test_set_S[i - 1])])
+    s = p == np.max(p)
+    s = s[0]
+    if p <= 0.5:
+        wrs = 1.0 * (wrs * (i - 1) + 1) / i
+    else:
+        wrs = 1.0 * (wrs * (i - 1)) / i
+print 'wrs', wrs
+wrj = 0
+for i in range(1, J_left + 1):
+    p = network.predict([Instance(test_set_J[i - 1])])
+    s = p == np.max(p)
+    s = s[0]
+    if p <= 0.5:
         wrj = 1.0 * (wrj * (i - 1) + 1) / i
     else:
         wrj = 1.0 * (wrj * (i - 1)) / i
